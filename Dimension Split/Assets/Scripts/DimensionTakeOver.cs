77902346c7fcbbd35c20f6ea94d248ac772b1ed9
divinekoala <divinekoala5@googlemail.com>
2016-03-22 19:33:45 +0000

    still working on camera stuff

---
+++ Dimension Split/Assets/Scripts/DimensionTakeOver.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DimensionTakeOver : MonoBehaviour {
+
+	public Camera dimensionOneCamera;
+	public Camera dimensionTwoCamera;
+
+	public float cameraSlowerMultipler;
+
+	public DimensionPreferance dp = DimensionPreferance.Neither;
+
+	void Update() {
+
+		if (Input.GetKeyDown(KeyCode.O)){
+			dp = DimensionPreferance.D1;
+			StartCoroutine("CameraMerge");
+		} else if (Input.GetKeyDown(KeyCode.P)){
+			dp = DimensionPreferance.D2;
+			StartCoroutine("CameraMerge");
+		} else if (Input.GetKeyDown(KeyCode.N)){
+			dp = DimensionPreferance.Neither;
+			StartCoroutine("CameraMerge");
+		}
+
+
+
+	}
+
+	IEnumerator CameraMerge () {
+		if (dp == DimensionPreferance.D1){
+			dimensionOneCamera.rect = new Rect (0f, Mathf.Lerp (0.5f, 0f, Time.time/cameraSlowerMultipler), 1f, 1f);
+			dimensionTwoCamera.rect = new Rect (0f, 0f, 1f, Mathf.Lerp (0.5f, 0f, Time.time/cameraSlowerMultipler));
+			yield return null;
+		} else if (dp == DimensionPreferance.D2){
+			dimensionOneCamera.rect = new Rect (0f, Mathf.Lerp (0.5f, 1f, Time.time/cameraSlowerMultipler), 1f, 1f);
+			dimensionTwoCamera.rect = new Rect (0f, 0f, 1f, Mathf.Lerp (0.5f, 1f, Time.time/cameraSlowerMultipler));
+			yield return null;
+		} else if (dp == DimensionPreferance.Neither) {
+			dimensionOneCamera.rect = new Rect (0f, Mathf.Lerp (0f, 0.5f, Time.time/cameraSlowerMultipler), 1f, 1f);
+			dimensionTwoCamera.rect = new Rect (0f, 0f, 1f, Mathf.Lerp (1f, 0.5f, Time.time/cameraSlowerMultipler));
+			yield return null;
+		}
+	}
+
+}
+
+public enum DimensionPreferance {
+	D1,
+	D2,
+	Neither
+}
---
+++ Dimension Split/Assets/Scripts/MyTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyTimer: MonoBehaviour{
+
+	public float time;
+	public float maxTimer = 1f;
+
+	bool timeOn;
+
+	void Update() {
+
+		if (timeOn)
+			time += Time.deltaTime;
+
+		if (time >= maxTimer){
+			time = maxTimer;
+			timeOn = false;
+		}
+	}
+
+	public void StartTimer(){
+		time = 0;
+		timeOn = true;
+	}
+
+	public float GetTime(){
+		return time;
+	}
+
+}
